2c18c2cf18de5fd49647ba85a82f3c5314d0c237
李辉 <lihui870920@gmail.com>
2023-09-08 11:19:11 +0800

    Nacos 动态配置

--- book-service/src/main/java/com/julan/book/controller/BookController.java
+++ book-service/src/main/java/com/julan/book/controller/BookController.java
@@ -1,5 +1,6 @@
 package com.julan.book.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.julan.book.service.impl.BooKServiceImpl;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/book")
 public class BookController {
 
+    @NacosValue("${custom.name}")
+    private String name;
+
     @Resource
     BooKServiceImpl booKService;
 
     @GetMapping("{id}")
     public Object index(@PathVariable("id") Long id) {
-        System.out.println("Book Service 8011");
+        System.out.println("Book Service 8011 Scope is " + name);
         return booKService.getBookById(id);
     }
 }
