96faffa38f9872925b38ff1891fbf6a701e4d4d9
李辉 <lihui870920@gmail.com>
2023-09-08 17:19:57 +0800

    Spring Custom Config

--- book-service/src/main/java/com/julan/book/controller/BookController.java
+++ book-service/src/main/java/com/julan/book/controller/BookController.java
@@ -1,26 +1,18 @@
 package com.julan.book.controller;
 
-
-
 import com.julan.book.service.impl.BooKServiceImpl;
 import jakarta.annotation.Resource;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/book")
-@RefreshScope
 public class BookController {
-    @Value(value = "${custom.name}")
-    public String name;
 
     @Resource
     BooKServiceImpl booKService;
 
-
     @GetMapping("{id}")
     public Object index(@PathVariable("id") Long id) {
-        System.out.println("Book Service 8011 Scope is " + name);
         return booKService.getBookById(id);
     }
 }
--- book-service/src/main/java/com/julan/book/service/impl/BooKServiceImpl.java
+++ book-service/src/main/java/com/julan/book/service/impl/BooKServiceImpl.java
@@ -1,12 +1,19 @@
 package com.julan.book.service.impl;
 
 import com.julan.book.service.BookService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 
 @Service
+@RefreshScope
 public class BooKServiceImpl implements BookService {
+
+    @Value(value = "${custom.name}")
+    public String name;
+
     @Override
     public Object getBookById(Long id) {
         HashMap<String, Object> book = new HashMap<>();
@@ -14,6 +21,7 @@ public class BooKServiceImpl implements BookService {
         book.put("name", "遥远的救世主");
         book.put("author", "豆豆");
         book.put("published_at", "2005年5月");
+        book.put("custom", name);
         return book;
     }
 }
